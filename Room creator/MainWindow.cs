7ebe862293f20b8af14e1a8ba330767d1557c22c
abraham <abrahamtellezv@gmail.com>
2023-03-30 09:12:15 -0600

    Commit and push before resetting pc

---
+++ Room creator/MainWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Room_creator
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow(string? jsonFilePath = null)
+        {
+            InitializeComponent();
+            _jsonFilePath = jsonFilePath;
+        }
+
+        readonly string? _jsonFilePath;
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            if (_jsonFilePath != null)
+            {
+
+            }
+        }
+    }
+}
--- Room creator/Program.cs
+++ Room creator/Program.cs
@@ -2,16 +2,13 @@ namespace Room_creator
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainWindow());
         }
     }
 }
